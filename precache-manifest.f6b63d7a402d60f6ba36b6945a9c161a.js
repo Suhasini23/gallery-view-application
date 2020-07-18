7134603ce7c5ce7fca9a085af74e4150260d712b
suhasini23 <ssumisingyan@gmail.com>
2020-07-19 03:04:48 +0530

    Updates

---
+++ precache-manifest.f6b63d7a402d60f6ba36b6945a9c161a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5621383fb64b72a9b8a11d301e14d703",
+    "url": "/mygallery/index.html"
+  },
+  {
+    "revision": "fda3eb7a7d7e5850b45b",
+    "url": "/mygallery/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "1f986a6e797e8779029b",
+    "url": "/mygallery/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fda3eb7a7d7e5850b45b",
+    "url": "/mygallery/static/js/2.f29d4efd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mygallery/static/js/2.f29d4efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f986a6e797e8779029b",
+    "url": "/mygallery/static/js/main.9392a8c1.chunk.js"
+  },
+  {
+    "revision": "da84c952bc383229fe17",
+    "url": "/mygallery/static/js/runtime-main.010aa384.js"
+  },
+  {
+    "revision": "113000a52f405721db421627a90af1f7",
+    "url": "/mygallery/static/media/logo.113000a5.png"
+  }
+]);
